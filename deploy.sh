88f90b784b9b327d367d2567d2e7cbea6eb17f3c
Tobias Bocanegra <tripod@adobe.com>
2025-01-26 15:00:41 +0100

    update

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+butler push export/web tripod/boebl:html5
+#butler push export/macos tripod/boebl:macos
+#butler push export/linux tripod/boebl:linux
+#butler push export/windows tripod/boebl:windows
