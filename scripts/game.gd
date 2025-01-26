c26e094ecc68e7e586dce0f897593b5d11cc3a6e
Wompking <magneto.tuvok@gmail.com>
2025-01-26 13:48:21 +0100

    the time crunch is real

--- scripts/game.gd
+++ scripts/game.gd
@@ -31,3 +31,7 @@ func load_level(nr:int, scn:PackedScene):
 
 func _on_play_pressed() -> void:
   show_level_select()
+
+
+func _on_nextlevel_pressed() -> void:
+  pass # Replace with function body.
